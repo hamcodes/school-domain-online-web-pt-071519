13ad9b2f13662367ed67ad71f63a4e4c7a2472cf
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-04 12:18:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,29 @@
-class School
-    def initialize(name)
-      @name = name
-      @roster = {}
+  def initialize(name) #why isn't grade here?
+    @name = name
+    @roster = {}
   end
-  
 
-  
-  
-  
-  
-  
+  def roster #why do you need a reader method for this?
+    @roster
+  end
+
+  def add_student(name, grade)
+    if
+      @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, students|
+        students.sort!
+    end
+  end
+
 end
